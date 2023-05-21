564b9f9d911066d6a57a46a5d9c8e3419973d949
Sayat <129245004+qwufy@users.noreply.github.com>
2023-05-22 01:16:44 +0600

    assignment5

--- BST/src/BST.java
+++ BST/src/BST.java
@@ -51,4 +51,46 @@ public class BST<K extends Comparable<K>, V> {
         else
             return node;
     }
+
+    public void delete(K key) {
+        root = delete(root, key);
+    }
+
+    private Node delete(Node node, K key) {
+        if (node == null)
+            return null;
+
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0)
+            node.left = delete(node.left, key);
+        else if (cmp > 0)
+            node.right = delete(node.right, key);
+        else {
+            if (node.left == null)
+                return node.right;
+            else if (node.right == null)
+                return node.left;
+            else {
+                Node minNode = findMinNode(node.right);
+                node.key = minNode.key;
+                node.value = minNode.value;
+                node.right = deleteMinNode(node.right);
+            }
+        }
+
+        return node;
+    }
+
+    private Node findMinNode(Node node) {
+        while (node.left != null)
+            node = node.left;
+        return node;
+    }
+
+    private Node deleteMinNode(Node node) {
+        if (node.left == null)
+            return node.right;
+        node.left = deleteMinNode(node.left);
+        return node;
+    }
 }
