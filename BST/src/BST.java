a39f98ef32093fef211706163e02f645b00281d2
Sayat <129245004+qwufy@users.noreply.github.com>
2023-05-22 01:16:14 +0600

    assignment5

--- BST/src/BST.java
+++ BST/src/BST.java
@@ -1,29 +1,54 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class BST<K extends Comparable<K>, V> {
     private Node root;
 
-    private class Node{
+    private class Node {
         private K key;
         private V value;
         private Node left, right;
-        public Node(K key, V value){
+
+        public Node(K key, V value) {
             this.key = key;
             this.value = value;
         }
     }
 
-    public void put(K key, V value){
-
+    public void put(K key, V value) {
+        root = put(root, key, value);
     }
 
-    public V get(K key){
+    private Node put(Node node, K key, V value) {
+        if (node == null)
+            return new Node(key, value);
 
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0)
+            node.left = put(node.left, key, value);
+        else if (cmp > 0)
+            node.right = put(node.right, key, value);
+        else
+            node.value = value;
+
+        return node;
     }
 
-    public void delete(K key){
-
+    public V get(K key) {
+        Node node = get(root, key);
+        return node != null ? node.value : null;
     }
 
-    public Iterable<K> iterator(){
+    private Node get(Node node, K key) {
+        if (node == null)
+            return null;
 
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0)
+            return get(node.left, key);
+        else if (cmp > 0)
+            return get(node.right, key);
+        else
+            return node;
     }
 }
